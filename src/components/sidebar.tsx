e89af7452971f02be5c9708b0e2e40f2fbc5c360
mohammed3200 <wwyuu799@gmail.com>
2024-11-13 00:03:42 +0200

    create project features and get project features and build ui for that

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,27 +1,26 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import { DottedSeparator } from './dotted-separator'
-import { Navigation } from './navigation'
-import { WorkspaceSwitcher } from './workspace-switcher'
-
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { DottedSeparator } from "./dotted-separator";
+import { Navigation } from "./navigation";
+import { WorkspaceSwitcher } from "./workspace-switcher";
+import { Projects } from "@/components";
 
 export const Sidebar = () => {
   return (
-    <aside className='h-full bg-neutral-100 p-4 w-full'>
-        <Link href="/" className="flex flex-row items-center px-4 gap-2">
-            <Image
-                src="/logo.svg"
-                alt='logo'
-                width={63}
-                height={63}
-            />
-            <p className="text-black lg:text-lg text-center font-black lg:-ml-4  ">Leadership Dashboard</p>
-        </Link>
-        <DottedSeparator className="my-4" />
-        <WorkspaceSwitcher />
-        <DottedSeparator className="my-4" />
-        <Navigation />
+    <aside className="h-full bg-neutral-100 p-4 w-full">
+      <Link href="/" className="flex flex-row items-center px-4 gap-2">
+        <Image src="/logo.svg" alt="logo" width={63} height={63} />
+        <p className="text-black lg:text-lg text-center font-black lg:-ml-4  ">
+          Leadership Dashboard
+        </p>
+      </Link>
+      <DottedSeparator className="my-4" />
+      <WorkspaceSwitcher />
+      <DottedSeparator className="my-4" />
+      <Navigation />
+      <DottedSeparator className="my-4" />
+      <Projects />
     </aside>
-  )
-}
+  );
+};
