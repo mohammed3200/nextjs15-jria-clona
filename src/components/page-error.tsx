36351cdde24c8f9ebccad87f0d792f6f14256ada
mohammed3200 <wwyuu799@gmail.com>
2025-02-15 13:50:16 +0200

    reduce animation size in PageError component

--- src/components/page-error.tsx
+++ src/components/page-error.tsx
@@ -11,7 +11,7 @@ interface PageErrorProps {
 const PageError = ({ message = "Something went wrong" }: PageErrorProps) => {
     return (
         <div className="flex flex-col items-center justify-center h-full">
-            <UseAnimations animation={error} size={56} wrapperStyle={{ padding: 100 }} />
+            <UseAnimations animation={error} size={20} wrapperStyle={{ padding: 100 }} />
             <p className="text-sm font-medium text-muted-foreground">{message}</p>
         </div>
     );
