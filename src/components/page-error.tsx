058a9da683bf17a9297666d265bcc316cb1c0b3c
mohammed3200 <wwyuu799@gmail.com>
2025-02-15 13:47:57 +0200

    add PageError and PageLoader components

---
+++ src/components/page-error.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import UseAnimations from 'react-useanimations';
+
+import error from 'react-useanimations/lib/error';
+
+interface PageErrorProps {
+    message: string
+}
+
+
+const PageError = ({ message = "Something went wrong" }: PageErrorProps) => {
+    return (
+        <div className="flex flex-col items-center justify-center h-full">
+            <UseAnimations animation={error} size={56} wrapperStyle={{ padding: 100 }} />
+            <p className="text-sm font-medium text-muted-foreground">{message}</p>
+        </div>
+    );
+};
+
+export default PageError;
